6576e57ed43837a27eab1a3734c35bf4afca7da5
unknown <hoaiphungcntt@gmail.com>
2019-06-05 11:02:17 +0700

    update

--- Session-1/ShowMenuRactangle/src/ShowMenuRactangle.java
+++ Session-1/ShowMenuRactangle/src/ShowMenuRactangle.java
@@ -2,24 +2,46 @@ import java.util.Scanner;
 
 public class ShowMenuRactangle {
     public static void main(String[] args) {
-//        System.out.println("   ---Menu---   ");
-//        System.out.println("1.Print the rectangle");
-//        System.out.println("2.Print the square triangle");
-//        System.out.println("3.Print isosceles triangle");
-//        System.out.println("Exit.");
+        int choice = -1;
         Scanner scanner = new Scanner(System.in);
-        int width, height;
-        System.out.print("nhập chiều rộng: ");
-        width = scanner.nextInt();
-        System.out.print("nhập chiều dài: ");
-        height = scanner.nextInt();
-        for (int i = 1; i <= height; i++) {
-            for (int j = 1; j <= width; j++) {
-                System.out.printf("*");
-                break;
+        do {
+            System.out.println("   ---Menu---   ");
+            System.out.println("1.Print the rectangle");
+            System.out.println("2.Print the square triangle");
+            System.out.println("3.Print isosceles triangle");
+            System.out.println("Exit.");
+            System.out.print("Nhập lựa chọn của bạn: ");
+            choice = scanner.nextInt();
+            switch (choice) {
+                case 1:
+                    int width, height;
+                    System.out.print("nhập chiều rộng: ");
+                    width = scanner.nextInt();
+                    System.out.print("nhập chiều dài: ");
+                    height = scanner.nextInt();
+                    for (int i = 1; i <= height; i++) {
+                        for (int j = 1; j <= width; j++) {
+                            System.out.print("*\t");
+                        }
+                        System.out.print("\n");
+                    }
+                    break;
+                case 2:
+                    System.out.println("Draw the triangle");
+                    System.out.println("  *** ***");
+                    System.out.println(" *********");
+                    System.out.println("  *******");
+                    System.out.println("   ****");
+                    System.out.println("    **");
+                    break;
+                case 3:
+                    System.out.println("ahihi");
+                    break;
+                case 0:
+                    System.exit(0);
+                default:
+                    System.out.print("Không có lựa chọn này");
             }
-            System.out.print("");
-        }
-//        System.out.print("\n");
+        } while (choice != 0);
     }
 }
---
+++ Session-2/FindMaxTwoArray/src/FindMaxTwoArray.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class FindMaxTwoArray {
+    public static void main(String[] args) {
+        int soDong,soCot;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhập vào số dòng của mảng: ");
+        soDong = scanner.nextInt();
+        System.out.print("Nhập vào số cột của mảng: ");
+        soCot = scanner.nextInt();
+        int arr[][] = new int[soDong][soCot];
+
+        System.out.println("Nhập các phần tử cho ma trận A:");
+        for (int i = 0; i < soDong; i++) {
+            for (int j = 0; j < soCot; j++) {
+                System.out.print("arr[" + i + "," + j + "] = ");
+                arr[i][j] = scanner.nextInt();
+            }
+        }
+        System.out.println("mảng arr là: ");
+        for (int i = 0; i < soDong; i++) {
+            for (int j = 0; j < soCot; j++) {
+                System.out.print(arr[i][j] + "\t");
+            }
+            System.out.println("");
+        }
+        int max = arr[0][0];
+        for (int i = 0; i < soDong; i++) {
+            for (int j = 0; j < soCot; j++) {
+                if (max < arr[i][j]) {
+                    max = arr[i][j];
+                }
+            }
+        }
+        System.out.println("Phần tử lớn nhất trong ma trận = " + max);
+    }
+}
